58f77d02e97f137623bd5b6c672008a3e33df6d3
Firepup Sixfifty <firepyp650@gmail.com>
2025-08-24 20:09:28 -0500

    Silly :3

---
+++ scripts/main.js
@@ -0,0 +1,53 @@
+// Utils
+
+function keysContains(map, value) {
+    let has = false;
+    map.each((k, _) => has = k === value);
+    return has;
+}
+
+function valuesContains(map, value) {
+    let has = false;
+    map.each((_, v) => has = v === value);
+    return has;
+}
+
+function keysFromValue(map, value) {
+    let keys = [];
+    map.each((k, v) => v === value && keys.push(k));
+    return keys;
+}
+
+// Main stuff
+
+let playerUnits = new ObjectMap();
+
+Time.runTask(0.1, function unitLoop() {
+    let cache = Groups.player;
+    let hasNull = cache.contains(p => p.unit() == null)
+    if (hasNull) {
+        cache.each(p => !!p.unit() && playerUnits.put(p, p.unit()));
+    } else {
+        playerUnits.clear();
+        cache.each(p => playerUnits.put(p, p.unit()))
+    }
+    Time.runTask(0.1, unitLoop);
+});
+
+Events.on(UnitDestroyEvent, event => {
+    print("SOMETHING DIED YAY!");
+    print("Player units right now:");
+    Groups.player.each(p => print(p + "->" + p.unit()));
+    print("Player units in cache:");
+    playerUnits.each((p, unit) => print(p + "->" + unit));
+    print("The dead unit:")
+    const u = event.unit;
+    print(u);
+    const icon = new TextureRegionDrawable(u.type.uiIcon);
+    const wasPlayer = valuesContains(playerUnits, u);
+    if (u.isPlayer() || wasPlayer) {
+        Vars.ui.hudfrag.showToast(icon, "<-- This unit was killed while being controlled by " + keysFromValue(playerUnits, u)[0].name + " :3");
+    }
+});
+
+print("main.js done loading");
